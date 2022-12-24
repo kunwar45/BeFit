e3972a08253691e0e734f3d59bb69e97884b8987
Kunwar Nir <knir45@gmail.com>
2022-12-24 14:12:38 -0500

    edited app;

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Title from './components/Title';
+import Post from './components/Post';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Title/>
+      <Post/>
       <StatusBar style="auto" />
     </View>
   );
