9882639e908f2af0663b70da2bbd25c4a532ae04
Kunwar Nir <knir45@gmail.com>
2022-12-24 14:23:34 -0500

    imports

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1D1E2C',
     alignItems: 'center',
     justifyContent: 'center',
   },
